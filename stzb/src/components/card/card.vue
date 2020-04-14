cb551113a5fe24a5caa3a2d283b07e5635213db8
liwei.liu <liwei.liu@mythware.com>
2020-04-14 08:05:34 +0800

    update

---
+++ stzb/src/components/card/card.vue
@@ -0,0 +1,48 @@
+<template>
+   <div class="content">
+       <div class="card-title">
+           {{title}}
+       </div>
+       <div class="card-main">
+           <div class="card-ide">
+               <img :src="userInfo.headerImg" alt="">
+               <p>
+                {{userInfo.name}}
+               </p>
+               <div>
+
+               </div>
+           </div>
+           <div class="showImg">
+               <img :src="userInfo.headerImg" alt="">
+           </div>
+           <div class="card-msg">
+               <div>
+                   {{brief}}
+               </div>
+           </div>
+       </div>
+       <div class="footer">
+            
+       </div>
+   </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            title:'中国水水倒萨大零秒十六点三顶顶顶顶顶了多少水水水水水水水水水水水水',
+            userInfo:{
+                name:'丁香医生',
+                headerImg:'https://profile.csdnimg.cn/9/2/9/3_xiasohuai'
+            },
+            brief:'的撒顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶大飒飒大苏打撒旦撒啊啊啊啊啊啊啊啊啊啊实打实大苏打据后期维护情况i外界保护你的撒顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶大飒飒大苏打撒旦撒啊啊啊啊啊啊啊啊啊啊实打实大苏打据后期维护情况i外界保护你'
+        }
+    }
+}
+</script>
+
+<style scoped lang='scss'>
+    @import './card.scss';
+</style>
---
+++ stzb/src/pages/home/index.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div class="msg">
+            <i-notice-bar closable>
+                点击"•●•"添加到我的小程序，快速寻找
+            </i-notice-bar> 
+      </div>
+      <div class="head">
+          <view>
+            <!-- 搜索 -->
+            <div class="search-bar">
+                <div class="search-bar__box ">
+                    <view @click ="showInput" class="search-bar__label">
+                        <icon class="icon-search" size="14" type="search" role="img"></icon>
+                        <view class="search-bar__text">搜索</view>
+                    </view> 
+                </div>
+            </div>
+           
+          </view>
+      </div>
+      <div class="home-card">
+          <card />
+      </div>
+    </div>
+</template>
+<script>
+import card from '@/components/card/card'
+export default {
+  components: {
+    card
+  }
+}
+</script>
+<style lang = "scss" scoped >
+   @import './index.scss';
+</style>
